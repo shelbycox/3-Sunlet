3d6c528a01b067d74b8ae7fa5a1fd1e6686b8cd8
shelbycox <aanford@gmail.com>
2023-04-18 17:01:11 -0400

    removed test code from ranks_ineq_gen.jl

--- ranks_ineq_gen.jl
+++ ranks_ineq_gen.jl
@@ -4,22 +4,6 @@ include("groups.jl")
 include("hyperplanes.jl")
 include("lambda_cyclic.jl")
 
-Z3 = FiniteCyclicGroup([3])
-H3 = generateSunletArr(Z3)
-
-s3 = sample(Z3, H3, 2^20)
-ranks3 = Dict()
-for s in keys(s3)
-    p = s3[s]
-    r = getRank(p, Z3)
-    I = getArrangementInequality(p, H3)
-    if r in keys(ranks3)
-        push!(ranks3[r], I)
-    else
-        ranks3[r] = [I]
-    end
-end
-
 function gen_ranks_ineq(group::FiniteCyclicGroup, N::Int64)
     H = generateSunletArr(group)
     S = sample(group, H, N)
@@ -37,5 +21,3 @@ function gen_ranks_ineq(group::FiniteCyclicGroup, N::Int64)
 
     return ranks
 end
-
-gen_ranks_ineq(Z3, 2^20)
