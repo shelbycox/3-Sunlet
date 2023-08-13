148b2329678186e84e86ced46f3eab1fc29afa6e
Shelby Cox <spcox@umich.edu>
2023-08-12 20:44:49 -0400

    added a file to test rendering graphs

--- ThreeSunlet/examples/row_reduced_ex.jl
+++ ThreeSunlet/examples/row_reduced_ex.jl
@@ -1,6 +1,18 @@
-inlcude("../src/row_reduction.jl")
+include("../src/row_reduction.jl")
 
 ## get the row reduction for all groups
 for group in GROUPS
     row_reduced_csv(group, "data/row_reduced/")
 end
+
+for group in GROUPS
+    L = lambdaGuess(group)
+    M = get_row_reduced_M(group)[1]
+    n = getGroupSize(group)
+    T2_end = length(get_tree_i_cols(L, group, 2))
+    println(group.structure, " ", LinearAlgebra.rank(M[1:3*n, 1:T2_end]), " ", LinearAlgebra.rank(M[(3*n+1):end,(T2_end + 1):end]))
+end
+
+group = FiniteCyclicGroup([6])
+M = get_row_reduced_M(group)[1]
+M[:, 1]
